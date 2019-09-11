49ff49e7fca93516bbf30ea32c55311632bbc0c7
caoyu <caoyupersonal@hotmail.com>
2019-09-11 20:36:51 +0800

    p937 solution

--- src/leetcode/solution/p918/Solution.java
+++ src/leetcode/solution/p918/Solution.java
@@ -3,7 +3,7 @@ package leetcode.solution.p918;
 public class Solution {
 
     public int maxSubarraySumCircular(int[] A) {
-        int maxValue = Integer.MIN_VALUE, sum = 0, maxSumSoFar = 0, minSumSoFar = 0,
+        int maxValue = -30000, sum = 0, maxSumSoFar = 0, minSumSoFar = 0,
                 maxSum = 0, minSum = 0;
 
         //获取sum和最大值
@@ -12,13 +12,22 @@ public class Solution {
             if (maxValue <= value) maxValue = value;
         }
 
+        //分情况处理
         if (maxValue > 0) {
 
             for (int value : A) {
-                maxSumSoFar = (maxSumSoFar + value) > value ? maxSumSoFar + value : value;
+//                maxSumSoFar = (maxSumSoFar + value) > value ? maxSumSoFar + value : value;
+                if (maxSumSoFar > 0)
+                    maxSumSoFar += value;
+                else
+                    maxSumSoFar = value;
                 if (maxSumSoFar > maxSum) maxSum = maxSumSoFar;
 
-                minSumSoFar = (minSumSoFar + value) < value ? minSumSoFar + value : value;
+//                minSumSoFar = (minSumSoFar + value) < value ? minSumSoFar + value : value;
+                if (minSumSoFar < 0)
+                    minSumSoFar += value;
+                else
+                    minSumSoFar = value;
                 if (minSumSoFar < minSum) minSum = minSumSoFar;
             }
 
--- src/leetcode/solution/p937/Solution.java
+++ src/leetcode/solution/p937/Solution.java
@@ -1,8 +1,34 @@
 package leetcode.solution.p937;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
     public String[] reorderLogFiles(String[] logs) {
-        
-        return new String[1];
+        Arrays.sort(logs, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int index1 = o1.indexOf(' ');
+                String s1 = o1.substring(index1 + 1);
+                int index2 = o2.indexOf(' ');
+                String s2 = o2.substring(index2 + 1);
+                char c1 = s1.charAt(0);
+                char c2 = s2.charAt(0);
+                if (Character.isLetter(c1) && Character.isLetter(c2)) {
+                    int contentCompare = s1.compareTo(s2);
+                    if (contentCompare == 0) {
+                        return o1.substring(0, index1).compareTo(o2.substring(0, index2));
+                    }else
+                        return contentCompare;
+                } else if (Character.isLetter(c1)) {
+                    return -1;
+                } else if (Character.isLetter(c2)) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        return logs;
     }
 }
